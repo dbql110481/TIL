c61c9f18ffd78e0908483c9bf81f7d270e42af89
sanghoon <dbql110481@naver.com>
2018-11-14 10:20:12 +0900

    commit

--- SYNCASYNC/sync.js
+++ SYNCASYNC/sync.js
@@ -3,40 +3,40 @@ getUser(1, gRepos);
 console.log('메인 코드 계속 진행중..');
 
 function gRepos (user) {
-    getRepos(u.githubID, gCommits);
+  getRepos(user.githubID, gCommits);
 }
 
 function gCommits(repos) {
-    getCommits(repos[0], displayCommits);
+  getCommits(repos[0], displayCommits);
 }
 
-function displayCommits(commits){
-    console.log(commits);
+function displayCommits(commits) {
+  console.log(commits);
 }
 
 function getUser(id, callback) {
-    const users = [
-        { id: 1, githubID: 'neo'},
-        { id: 2, githubID: 'john'}
-    ];
-    setTimeout(() => {
-        console.log('Reading Data from DB');
-        const user = users.find(user => user.id === id);
-        //Ready!
-        callback(user);
-    },2000);
+  const users = [
+    { id: 1, githubID: 'neo'},
+    { id: 2, githubID: 'john'}
+  ]
+  setTimeout(() => {
+    console.log('Reading Data from DB');
+    const user = users.find(user => user.id === id);
+    // Ready!
+    callback(user);
+  }, 2000)
 }
 
-function getRepos (userID) {
-    console.log(`Finding [ ${userID} ]'s all github repo...`);
-    setTimeout(()=> {
-        callback(['TIL', 'ES6', 'Express-demo']);
-    }, 1500);
+function getRepos (userID, callback) {
+  console.log(`Finding [ ${userID} ]'s all github repo...`);
+  setTimeout(()=>{
+    callback(['TIL', 'ES6', 'Express-demo']);
+  }, 1500)
 }
 
-function getCommits (repo) {
-    console.log(`Getting all commits in [ ${repo} ]`);
-    setTimeout(()=> {
-        callback(['Init repo', 'Finish Something']);
-    }, 2000);
+function getCommits (repo, callback) {
+  console.log(`Getting all commits in [ ${repo} ]`);
+  setTimeout(()=>{
+    callback(['Init repo', 'Finish Something'])
+  }, 2000);
 }
--- express-advanced/routes/movies.js
+++ express-advanced/routes/movies.js
@@ -3,83 +3,71 @@ const router = express.Router();
 const Joi = require('joi');
 
 const movies = [
-    { id: 1, title: 'Bohemian Rhapsody'},
-    { id: 2, title: 'Matrix'},
-    { id: 3, title: 'Edge of Tommorow'}
-]
+  { id: 1, title: 'Bohemian Rhapsody' },
+  { id: 2, title: 'Matrix' },
+  { id: 3, title: 'Edge of Tommorow' },
+];
 
-/* GET /api/movies/1 */
-router.get('/api/movies', (req, res) => {
-    res.send(movies);
+/* GET /api/movies */
+router.get('/', (req, res) => {
+  res.send(movies);
 });
 
 /* GET /api/movies/1 */
-router.get('/api/movies/:id', (req, res) => {
-    const movie = movies.find((movie) => {
-        return movie.id === parseInt(req.params.id);
-    });
-    if(!movie){
-        res.status(404).send(`Movie with given id(${req.params.id}) is not found`);
-    }
-    res.send(movie);
+router.get('/:id', (req, res) => {
+  const movie = getMovie(movies, parseInt(req.params.id));
+  if (!movie) res.status(404).send(`Movie with given id(${req.params.id}) is not found.`);
+  res.send(movie);
 });
 
-/* POST /api/movies/1 */
-router.post('/api/movies', (req, res) => {
-    const schema = {
-        title: Joi.string().min(2).required(),
-    }
+/* POST /api/movies */
+router.post('/', (req, res) => {
+  const { error } = validateMovie(req.body)
 
-    const result = Joi.validate(req.body, schema);
-    console.log(result);
+  if (error) return res.status(400).send(error.message);
+  
+  const movie = {
+    id: movies.length + 1,
+    title: req.body.title
+  };
 
-    if(result.error){
-        res.status(400).send(result.error.message);
-    }
-
-    const movie = {
-        id: movies.length + 1,
-        title: req.body.title
-    };
-    movies.push(movie);
-    res.send(movies);
+  movies.push(movie);
+  res.send(movie);
 });
 
 /* PUT /api/movies/1 */
-router.put('/api/movies/:id', (req, res) => {
-    const movie = movies.find(movie => movie.id === parseInt(req.params.id))
+router.put('/:id', (req, res) => {
+  const movie = getMovie(movies, parseInt(req.params.id));
+  if (!movie) return res.status(404).send(`The movie with the given ID(${req.params.id}) was not found`);
+  
+  const { error } = validateMovie(req.body)
 
-    if(!movie) {
-        return res.status(404).send(`The movie with the given ID(${req.params.id}) was not found`)
-    }
+  if (error) return res.status(400).send(error.message);
 
-    const schema = {
-        title: Joi.string().min(2).required(),
-    }
-
-    const result = Joi.validate(req.body, schema);
-
-    if(result.error) {
-        return res.status(400).send(result.error.message);
-    }
-
-    movie.title = req.body.title;
-    res.send(movie);
+  movie.title = req.body.title;
+  res.send(movie);
 });
 
 /* DELETE /api/movies/1 */
-router.delete('/api/movies/:id', (req, res) => {
-    const movie = movies.find((movie) => {
-        return movie.id === parseInt(req.params.id);
-    });
+router.delete('/:id', (req, res) => {
+  const movie = getMovie(movies, parseInt(req.params.id));
+  if (!movie) return res.status(404).send(`The movie with the given ID(${req.params.id}) was not found`);
 
-    if(!movie) {
-        return res.status(404).send(`The movie with the given ID(${req.params.id}) was not found`)
-    }
-    const index = movies.indexOf(movie);
-    movies.splice(index, 1);
+  const index = movies.indexOf(movie);
+  movies.splice(index, 1);
 
-    res.send(movie);    
+  res.send(movie);
 });
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().min(2).required(),
+  }
+  return Joi.validate(movie, schema);
+}
+
+function getMovie(movies, id){
+  return movies.find(movie => movie.id === id)
+}
 
 module.exports = router;
